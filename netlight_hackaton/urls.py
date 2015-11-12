f90025a6fef643dfa25f4a5eb34c20532436dc67
Martin Abelson Sahlen <Sahlen@Martins-MacBook-Pro-2.local>
2015-11-13 00:44:40 +0100

    Add rest method

--- netlight_hackaton/urls.py
+++ netlight_hackaton/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from netlight_hackaton.api.views import get_shit
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^shit/', get_shit),
 ]
